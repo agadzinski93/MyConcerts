2f1e6598f01458862bd979f99b79c5becba8cae6
Adam <galaxytoeternity@yahoo.com>
2021-02-16 00:29:29 -0800

    Added CRUD functionality

---
+++ seeds/seedHelpers.js
@@ -0,0 +1,19 @@
+module.exports.descriptors = [
+    "Standard",
+    "Hyper",
+    "Relaxed",
+    "Mixed",
+    "Super",
+    "Flashy",
+    "Calm",
+    "Moody"
+]
+
+module.exports.places = [
+    "Central",
+    "Bash",
+    "Topic",
+    "Rave",
+    "Party",
+    "Club"
+]
