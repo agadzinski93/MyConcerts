f7f3de357b71c1f987cecbbdd165b648adfd4dc9
agadzinski93 <agadzinski93@users.noreply.github.com>
2022-04-26 17:15:16 -0700

    Fixed Responsive CSS and Client JS Errors

--- controllers/user.js
+++ controllers/user.js
@@ -1,7 +1,20 @@
+const ExpressError = require('../utilities/ExpressError');
 const ObjectId = require('mongoose').Types.ObjectId;
 const User = require('../models/user');
+const Concert = require('../models/concert');
 
 module.exports = {
+    async renderUser(req,res,next) {
+        try {
+            const userID = req.params.id;
+            const user = await User.findById(userID)
+            .populate({path:'followers', model:'user', select:'username image'});
+            const concerts = await Concert.find({author:{$eq:ObjectId(userID)}}, {"title":1,"price":1,"location":1,"image":1,"description":1});
+            res.render('users/details', {user, concerts});
+        } catch(e) {
+            next(new ExpressError("User Does Not Exist",400));
+        }
+    },
     async followUser(req, res) {
         const currentUserId = req.body.myId;
         const authorId = req.body.authorId;
---
+++ public/js/userPage.js
@@ -0,0 +1,110 @@
+async function followUser(e) {
+    e.stopPropagation();
+    let result = await fetch('/follow', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            myId: userId,
+            authorId: authorId,
+        })
+    });
+    updateFollowStatus(true, true);
+    addFollower();
+};
+async function unFollowUser(e) {
+    e.stopPropagation();
+    let result = await fetch('/follow', {
+        method: 'DELETE',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            myId: userId,
+            authorId: authorId,
+        })
+    });
+    updateFollowStatus(false, true);
+    removeFollower();
+};
+/**
+ * 
+ * @param {bool} isFollowing - is user following the concert author
+ * @param {bool} afterPageLoad - Altering visibility of buttons after page already loaded
+ */
+function updateFollowStatus(isFollowing, afterPageLoaded = false) {
+    let btnFollow = document.getElementById('follow');
+    let btnUnfollow = document.getElementById('unfollow');
+    if (isFollowing) {
+        if (afterPageLoaded) {
+            btnFollow.removeEventListener('click', followUser);
+            btnFollow.classList.toggle('hide');
+            btnUnfollow.classList.toggle('hide');
+        }
+        else {
+            btnFollow.classList.toggle('hide');
+        }
+        btnUnfollow.addEventListener('click', unFollowUser);
+    }
+    else {
+        if (afterPageLoaded) {
+            btnUnfollow.removeEventListener('click', unFollowUser);
+            btnUnfollow.classList.toggle('hide');
+            btnFollow.classList.toggle('hide');
+        }
+        else {
+            btnUnfollow.classList.toggle('hide');
+        }
+        btnFollow.addEventListener('click', followUser);
+    }
+}
+function hideFollowButtons() {
+    let btnFollow = document.getElementById('follow');
+    let btnUnfollow = document.getElementById('unfollow');
+    btnFollow.classList.toggle('hide');
+    btnUnfollow.classList.toggle('hide');
+}
+function addFollower() {
+    if (document.querySelector('.followersContainer > p') != null) {
+        document.querySelector('.followersContainer > p').remove();
+    }
+
+    let followerTag = document.createElement('div');
+    followerTag.classList.toggle('follower');
+    followerTag.setAttribute('id', currentUser);
+
+    let imageTag = document.createElement('div');
+    imageTag.classList.toggle('profileImage');
+    imageTag.style.backgroundImage = "url('" + userImage + "')";
+
+    let userTag = document.createElement('p');
+    userTag.textContent = currentUser;
+
+    followerTag.appendChild(imageTag);
+    followerTag.appendChild(userTag);
+    
+    document.querySelector('.followersContainer').appendChild(followerTag);
+    
+}
+function removeFollower() {
+    if (document.querySelector('.followersContainer').childElementCount === 1) {
+        let noUsers = document.createElement('p');
+        noUsers.textContent = author + " has no followers yet";
+        document.getElementById(currentUser).remove();
+        document.querySelector('.followersContainer').appendChild(noUsers);
+    }
+    else {
+        document.getElementById(currentUser).remove();
+    }
+    
+}
+
+if (!userAuthorMatch && isLoggedIn) {
+    updateFollowStatus(following);
+}
+else {
+    if (isLoggedIn) {
+        hideFollowButtons();
+    }
+}
